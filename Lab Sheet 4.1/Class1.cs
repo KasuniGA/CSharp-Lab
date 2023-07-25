9a38b392c5112dc3162dd4c81e1513c1c7b280ec
KasuniGA <135317655+KasuniGA@users.noreply.github.com>
2023-07-25 10:52:25 +0530

    Add files via upload

---
+++ Lab Sheet 4.1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_01
+    {
+        public class ConvertValues
+        {
+            public void KilometerToMeter()
+            {
+                Console.Write("Enter the distance in kilometers (km): ");
+                string input = Console.ReadLine();
+
+                if (double.TryParse(input, out double kilometers))
+                {
+                    double meters = kilometers * 1000;
+                    Console.WriteLine($"{kilometers} kilometers is equal to {meters} meters.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+        }
+    }
---
+++ Lab Sheet 4.1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_01
+    {
+        public class Program
+        {
+            public static void Main()
+            {
+                ConvertValues converter = new ConvertValues();
+                converter.KilometerToMeter();
+            }
+        }
+    }
