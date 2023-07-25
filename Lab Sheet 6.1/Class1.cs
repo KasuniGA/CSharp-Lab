11a658a5c924947acc6068dc1cd9d9edb2a39536
KasuniGA <135317655+KasuniGA@users.noreply.github.com>
2023-07-25 11:30:27 +0530

    Add files via upload

---
+++ Lab Sheet 6.1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_01
+{
+    public class ArrayProcessor
+    {
+        public int[] CreateArrayWithZeroPadding(int size)
+        {
+            int[] array = new int[size * 2]; // Double the size to accommodate zero padding.
+            return array;
+        }
+    }
+
+}
---
+++ Lab Sheet 6.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_01
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the size of the array: ");
+            if (int.TryParse(Console.ReadLine(), out int size) && size > 0)
+            {
+                ArrayProcessor arrayProcessor = new ArrayProcessor();
+                int[] array = arrayProcessor.CreateArrayWithZeroPadding(size);
+
+                Console.WriteLine("Enter values for the array:");
+                for (int i = 0; i < size; i++)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int value))
+                    {
+                        array[i] = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid integer value.");
+                        i--;
+                    }
+                }
+
+                Console.WriteLine("\nValues inside the array:");
+                foreach (int element in array)
+                {
+                    Console.Write($"{element} ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid positive integer for the array size.");
+            }
+        }
+    }
+
+}
