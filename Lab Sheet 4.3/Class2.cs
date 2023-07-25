f1b79036df95325b18115265935ff034c0cbf2b2
KasuniGA <135317655+KasuniGA@users.noreply.github.com>
2023-07-25 11:16:54 +0530

    Add files via upload

---
+++ Lab Sheet 4.3/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            double area = Math.PI * radius * radius;
+            double circumference = 2 * Math.PI * radius;
+
+            Console.WriteLine("area: " + area);
+            Console.WriteLine("circumference: " + circumference);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Lab Sheet 4.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_03
+{
+    public static void Main()
+    {
+        Console.Write("Enter the distance in kilometers (km): ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double kilometers))
+        {
+            ConvertValues converter = new ConvertValues();
+            double meters = converter.KilometerToMeter(kilometers);
+            Console.WriteLine($"{kilometers} kilometers is equal to {meters} meters.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
