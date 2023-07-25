12a63cfb5a32c3341257a667183238c5564bc8e1
KasuniGA <135317655+KasuniGA@users.noreply.github.com>
2023-07-25 11:10:59 +0530

    Add files via upload

---
+++ Lab Sheet 4.3/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_03
+{
+    public class ConvertValues
+    {
+        public double KilometerToMeter(double kilometers)
+        {
+            double meters = kilometers * 1000;
+            return meters;
+        }
+    }
+}
