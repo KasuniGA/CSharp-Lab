2d8d84b48655bfd3b1da34774677637cd28562ee
KasuniGA <135317655+KasuniGA@users.noreply.github.com>
2023-07-26 09:24:54 +0530

    Add files via upload

---
+++ Lab Sheet 5.5/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_02
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] array = new int[10];
+            Console.WriteLine("Enter 10 integer values:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int value))
+                    array[i] = value;
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer value.");
+                    i--;
+                }
+            }
+
+            ArrayOperations arrayOps = new ArrayOperations();
+
+            int minimumValue = arrayOps.FindMinimum(array);
+            int maximumValue = arrayOps.FindMaximum(array);
+            double averageValue = arrayOps.FindAverage(array);
+            int[] reversedArray = arrayOps.ReverseArray(array);
+
+            Console.WriteLine($"Minimum value: {minimumValue}");
+            Console.WriteLine($"Maximum value: {maximumValue}");
+            Console.WriteLine($"Average value: {averageValue:F2}");
+
+            Console.WriteLine("Array in reverse order:");
+            for (int i = 0; i < reversedArray.Length; i++)
+            {
+                Console.Write($"{reversedArray[i]} ");
+            }
+        }
+    }
+
+}
---
+++ Lab Sheet 5.5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_02
+{
+    using System;
+
+    public class ArrayOperations
+    {
+        public int FindMinimum(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                    min = arr[i];
+            }
+            return min;
+        }
+
+        public int FindMaximum(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            return max;
+        }
+
+        public double FindAverage(int[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum / arr.Length;
+        }
+
+        public int[] ReverseArray(int[] arr)
+        {
+            int[] reversedArray = new int[arr.Length];
+            int j = 0;
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                reversedArray[j] = arr[i];
+                j++;
+            }
+
+            return reversedArray;
+        }
+    }
+
+}
