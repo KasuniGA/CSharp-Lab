e74172e5862dfb6d3f4da65cc9aa5860414b5859
KasuniGA <135317655+KasuniGA@users.noreply.github.com>
2023-07-26 09:23:57 +0530

    Add files via upload

---
+++ Lab Sheet 5.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Question_01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_01
+{
+
+    public class SeparateClass
+    {
+        private void SayHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
+
+// question 02//
+
+
+
+public class Program
+{
+    public static void Main()
+    {
+        SeparateClass separateObj = new SeparateClass();
+        separateObj.SayHello();
+    }
+}
